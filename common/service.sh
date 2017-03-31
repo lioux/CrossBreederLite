8fb4bd532f5e392a5cdf32e8c6852cc0a949a66e
Rorsch_X1 <blackm3285@gmail.com>
2017-03-31 16:34:44 +0800

    Add files via upload

---
+++ common/service.sh
@@ -0,0 +1,15 @@
+#!/system/bin/sh
+# Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
+# This will make your scripts compatible even if Magisk change its mount point in the future
+MODDIR=${0%/*}
+
+# This script will be executed in late_start service mode
+# More info in the main Magisk thread
+
+/system/etc/CrossBreeder/zzCrossBreeder
+/system/etc/CrossBreeder/zzCHECK_NET_DNS
+/system/etc/CrossBreeder/SQLITE
+/system/etc/CrossBreeder/GameVM
+/system/etc/CrossBreeder/dynlmk
+/system/etc/CrossBreeder/dynzram
+/system/etc/CrossBreeder/dynbsd
