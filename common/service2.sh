aedbf58f75f760da592ee9d488a840d7e6ea9eb2
Rorsch_X1 <blackm3285@gmail.com>
2017-04-01 11:40:47 +0800

    Add files via upload

--- common/service2.sh
+++ common/service2.sh
@@ -6,9 +6,11 @@ MODDIR=${0%/*}
 # This script will be executed in late_start service mode
 # More info in the main Magisk thread
 
-/system/etc/CrossBreeder/zzCrossBreeder
-/system/etc/CrossBreeder/zzCHECK_NET_DNS
-/system/etc/CrossBreeder/FDE_Governor_Tweaks
-/system/etc/CrossBreeder/GameVM
-/system/etc/CrossBreeder/SQLITE
-#/system/etc/CrossBreeder/dynbsd
+while [ -f /system/etc/CrossBreeder/dynlmk ] && [ -f /system/etc/CrossBreeder/dynzram ]; do
+
+/system/etc/CrossBreeder/dynlmk
+/system/etc/CrossBreeder/dynzram
+
+sleep 60
+
+done
